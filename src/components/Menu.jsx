8d9fedac1e4b546ea64a42ff54f2bb9bf4d9e683
bootupdesigner <bootup.itinfo@gmail.com>
2022-06-08 21:39:42 -0400

    added horizontal lines to menu, placed topics in dropdown

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -24,32 +24,48 @@ const Menu = () => {
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
                   <Nav.Link href="/">Introduction</Nav.Link>
-                  <Nav.Link href="/self-worth">Self-Worth</Nav.Link>
-                  <Nav.Link href="positude">Positude</Nav.Link>
-                  <Nav.Link href="etiquette">Etiquette</Nav.Link>
-                  <Nav.Link href="health-and-hygiene">Health ad Hygiene</Nav.Link>
-                  <Nav.Link href="physical-activity-and-nutrition">Physical Activity and Nutrtion</Nav.Link>
-                  <Nav.Link href="your-body">Your Body</Nav.Link>
-                  <Nav.Link href="dating-and-sex">Dating and Sex</Nav.Link>
-                  <Nav.Link href="safety">Safety</Nav.Link>
-                  <Nav.Link href="closing">Closing</Nav.Link>
-                  <Nav.Link href="hotlines">Hotlines</Nav.Link>
-                  <Nav.Link href="websites">Websites</Nav.Link>
+                  <hr />
                   <NavDropdown
-                    title="Dropdown"
+                    title="Topics"
                     id={`offcanvasNavbarDropdown-expand-${expand}`}
                   >
                     <NavDropdown.Item >
+                      <Link to="/self-worth">Self-Worth</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item >
+                      <Link to="positude">Positude</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item >
+                      <Link to="etiquette">Etiquette</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item >
+                      <Link to="health-and-hygiene">Health ad Hygiene</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item >
+                      <Link to="physical-activity-and-nutrition">Physical Activity and Nutrtion</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item >
+                      <Link to="your-body">Your Body</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item >
+                      <Link to="dating-and-sex">Dating and Sex</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item >
                       <Link to="safety">Safety</Link>
                     </NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">
-                      Another action
+                    <NavDropdown.Item >
+                      <Link to="closing">Closing</Link>
                     </NavDropdown.Item>
+
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="#action5">
                       Something else here
                     </NavDropdown.Item>
                   </NavDropdown>
+                  <hr />
+                  <Link to="hotlines">Hotlines</Link>
+                  <hr />
+                  <Link to="websites">Websites</Link>
                 </Nav>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
